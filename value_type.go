c6972c8ec6d4bbfdcad54c8f91efbfdac22c69ee
Erik Hollensbe <github@hollensbe.org>
2016-10-23 18:33:22 +0000

    value_type.go: replace iota comments with descriptions of each type
    makes golint happy (and yields better docs I think)
    
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- value_type.go
+++ value_type.go
@@ -5,29 +5,54 @@ package mruby
 type ValueType uint32
 
 const (
-	TypeFalse     ValueType = iota // 0
-	TypeFree                       // 1
-	TypeTrue                       // 2
-	TypeFixnum                     // 3
-	TypeSymbol                     // 4
-	TypeUndef                      // 5
-	TypeFloat                      // 6
-	TypeCptr                       // 7
-	TypeObject                     // 8
-	TypeClass                      // 9
-	TypeModule                     // 10
-	TypeIClass                     // 11
-	TypeSClass                     // 12
-	TypeProc                       // 13
-	TypeArray                      // 14
-	TypeHash                       // 15
-	TypeString                     // 16
-	TypeRange                      // 17
-	TypeException                  // 18
-	TypeFile                       // 19
-	TypeEnv                        // 20
-	TypeData                       // 21
-	TypeFiber                      // 22
-	TypeMaxDefine                  // 23
-	TypeNil       ValueType = 0xffffffff
+	// TypeFalse is `false`
+	TypeFalse ValueType = iota
+	// TypeFree is ?
+	TypeFree
+	// TypeTrue is `true`
+	TypeTrue
+	// TypeFixnum is fixnums, or integers for this case.
+	TypeFixnum
+	// TypeSymbol is for entities in ruby that look like `:this`
+	TypeSymbol
+	// TypeUndef is a value internal to ruby for uninstantiated vars.
+	TypeUndef
+	// TypeFloat is any floating point number such as 1.2, etc.
+	TypeFloat
+	// TypeCptr is a void*
+	TypeCptr
+	// TypeObject is a standard ruby object, base class of most instantiated objects.
+	TypeObject
+	// TypeClass is the base class of all classes.
+	TypeClass
+	// TypeModule is the base class of all Modules.
+	TypeModule
+	// TypeIClass is ?
+	TypeIClass
+	// TypeSClass is ?
+	TypeSClass
+	// TypeProc are procs (concrete block definitons)
+	TypeProc
+	// TypeArray is []
+	TypeArray
+	// TypeHash is { }
+	TypeHash
+	// TypeString is ""
+	TypeString
+	// TypeRange is (0..x)
+	TypeRange
+	// TypeException is raised when using the raise keyword
+	TypeException
+	// TypeFile is for objects of the File class
+	TypeFile
+	// TypeEnv is for getenv/setenv etc
+	TypeEnv
+	// TypeData is ?
+	TypeData
+	// TypeFiber is for members of the Fiber class
+	TypeFiber
+	// TypeMaxDefine is ?
+	TypeMaxDefine
+	// TypeNil is nil
+	TypeNil ValueType = 0xffffffff
 )
